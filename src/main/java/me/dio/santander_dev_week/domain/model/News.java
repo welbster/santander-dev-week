131fa1bb70c3c1735f54aae9cb519883fe23407d
Welbster Uchoas <welbsteruchoas@gmail.com>
2024-06-22 14:17:54 -0300

    Initial commit

---
+++ src/main/java/me/dio/santander_dev_week/domain/model/News.java
@@ -0,0 +1,8 @@
+package me.dio.santander_dev_week.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
---
+++ src/main/java/me/dio/santander_dev_week/domain/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package me.dio.santander_dev_week.domain.repository;
+
+import me.dio.santander_dev_week.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String number);
+
+    boolean existsByCardNumber(String number);
+}
